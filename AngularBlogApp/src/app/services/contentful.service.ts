d9be9e0b30c6b846c3725dd041dc916e749741e3
Ravindra <saroderavinrda95@gmail.com>
2023-09-03 09:37:32 +0530

    AngularBlogAPP

---
+++ AngularBlogApp/src/app/services/contentful.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { createClient, Entry } from 'contentful';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentfulService {
+
+  constructor() { }
+
+  private client = createClient({
+    space: 'b8o51n8w4lvb',
+    accessToken: '7J75kmZoCIZsypbp_p7uJCb886XY8PNeX0mv9C6FJR4'
+  });
+
+  getAllEntries(){
+    const promise = this.client.getEntries()
+    return from(promise);
+  }
+
+  getEntryById(id: string){
+    const promise = this.client.getEntry(id);
+    return from(promise);
+    
+  }
+}
